90ed0ddaa839b7017fa4494b892108cdec92e7c8
Farn <irfankechi7@gmail.com>
2024-03-11 17:38:36 +0700

    install auto routes

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+  <div style="width: 100%; height: 100%; display: block;">
+    <a href="https://vitejs.dev" target="_blank" style="width: 100%; height: 100%; display: block;">
+      <img style="height: 200px; width: 200px;" src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img style="height: 200px; width: 200px;" src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <<router-view/>
+  <router-view/>
 </template>
